3ea68b3aa540344b315d077788311a8028772049
wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2025-04-22 11:15:33 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function abrirChat() {
+    document.getElementById('chatBox').style.display = 'block';
+    document.getElementById('openChatBtn').style.display = 'none';
+  }
+
+  function fecharChat() {
+    document.getElementById('chatBox').style.display = 'none';
+    document.getElementById('openChatBtn').style.display = 'inline-block';
+  }
